d70374744da4e4744a13779fcf44c51e4049c9d0
Jimmy <jabez007@users.noreply.github.com>
2022-09-01 08:50:22 -0500

    cards layout

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,35 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <va-card class="wrapper" color="dark" gradient>
-      <HelloWorld msg="You did it!" />
-    </va-card>
-  </header>
-
-  <main>
-    
-  </main>
+  <div class="row justify--space-evenly">
+    <va-progress-circle v-if="loading" indeterminate />
+    <div v-for="(t, k) in types" :key="k" class="flex sm2">
+      <va-card class="item" color="dark" gradient>
+        {{ t.name }}
+      </va-card>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { getResistantTypes, generateTeams } from "./lib/pokedex.js";
+
+export default {
+  data: () => ({
+    loading: false,
+    types: [],
+  }),
+  created() {
+    this.loading = true;
+    const self = this;
+    getResistantTypes().then((data) => {
+      self.types.splice(0, self.types.length, ...data.slice(0, 30));
+      self.loading = false;
+    });
+  },
+};
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.item {
+  margin: 1rem;
 }
 </style>
