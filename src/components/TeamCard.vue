b1123fa2875b2aefab6586b785bd51d2a25dae80
Jimmy <jabez007@users.noreply.github.com>
2022-09-02 11:39:46 -0500

    center some things

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -4,14 +4,16 @@
       <type-chip v-for="tn in types" :key="tn" :type="tn" />
     </va-card-title>
     <va-divider>
-      <span class="px-2">Total Stats Score: {{ (team.score || 0).toFixed(4) }}</span>
+      <span class="px-2"
+        >Total Stats Score: {{ (team.score || 0).toFixed(4) }}</span
+      >
     </va-divider>
     <va-card-content class="row justify--space-evenly">
       <va-card color="_dark" v-for="p in team.pokemon" :key="p.name">
-        <va-card-title>
+        <va-card-title class="justify--space-evenly">
           <type-chip v-for="pt in p.types" :key="pt" :type="pt" size="small" />
         </va-card-title>
-        <va-card-content>
+        <va-card-content class="row justify--center">
           <va-avatar color="background" :src="p.sprite" />
         </va-card-content>
       </va-card>
